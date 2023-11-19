73d8a49ec2ae98a18b06b35b8e64dde75533cdf9
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-11-18 22:26:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function rotate(value) {
+  document.getElementById('beer').style.webkitheight="" + value + "%";
+  document.getElementById('beer').style.msheight="" + value + "%";
+  document.getElementById('beer').style.Mozheight="" + value + "%";
+  document.getElementById('beer').style.Oheight="" + value + "%";
+  document.getElementById('beer').style.height="" + value + "%";
+}
